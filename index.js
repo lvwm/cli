6fb123484b59625552fe2cc5de7e932827e291c7
lvwm <81692813+lvwm@users.noreply.github.com>
2021-04-02 20:36:25 +0800

    feat: chrome command add url

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const { program } = require("commander");
 
 program.version("0.0.1");
+
+program
+  .command("chrome [name]")
+  .description("use chrome to open specified urls")
+  .option("-a, --add", "add url to list")
+  .option("--name <name>", "site name")
+  .option("--url <url>", "site url")
+  .action((name, options) => {
+    require("./lib/chrome")(name, options);
+  });
+
 program.parse(process.argv);
