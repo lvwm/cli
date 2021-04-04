91528a9147fb676a6202680ed71c1fba026f0be3
lvwm <81692813+lvwm@users.noreply.github.com>
2021-04-04 19:57:52 +0800

    feat: chrome command support multi-urls

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ program.version("0.0.1");
 program.usage('command [options]')
 
 program
-  .command("chrome [name]")
+  .command("chrome [name...]")
   .usage("[name] [options]")
   .description("use chrome to open specified urls")
   .option("-a, --add", "add url to list")
