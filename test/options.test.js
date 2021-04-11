1b7eb8e47cd8e152466c83bb388396a2324d07fd
lvwm <81692813+lvwm@users.noreply.github.com>
2021-04-11 20:08:51 +0800

    test: load options from config file

--- test/options.test.js
+++ test/options.test.js
@@ -5,3 +5,8 @@ test('save options to config file', () => {
   options.save(data)
   expect(options.load()).toStrictEqual({});
 });
+
+test('load options from config file', () => {
+  const data = options.load();
+  expect(options.load()).toStrictEqual({});
+});
