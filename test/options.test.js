f8e2fd4b955d7ef42025c1114dbcef90cfc9905e
lvwm <81692813+lvwm@users.noreply.github.com>
2021-04-12 14:34:26 +0800

    test: add url to options

--- test/options.test.js
+++ test/options.test.js
@@ -10,3 +10,12 @@ test('load options from config file', () => {
   const data = options.load();
   expect(options.load()).toStrictEqual({});
 });
+
+test('add a url to options', () => {
+  const item = {
+    name: 'npmjs',
+    url: 'https://npmjs.com'
+  }
+  options.urls = [item];
+  expect(options.load().chrome.urls).toStrictEqual([item])
+})
